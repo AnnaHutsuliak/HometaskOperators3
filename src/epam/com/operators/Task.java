2a3845d37e024955941dcfb0bdf5c943fc8239b0
Anna Hutsuliak <anhutsuliak@gmail.com>
2020-01-14 22:42:31 +0200

    Initial commit

---
+++ src/epam/com/operators/Task.java
@@ -0,0 +1,10 @@
+package epam.com.operators;
+
+public class Task {
+    public static void main(String[] args) {
+        int i = 0; int j = 0;
+        while ((j= 0)!=1 && i++<10)
+            while (j<=10)
+            System.out.print(j++ !=10 ? (i>j ? " " : "*") : "\n");
+        }
+    }
